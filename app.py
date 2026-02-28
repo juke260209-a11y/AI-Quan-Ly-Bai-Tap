a37d5a9e446bc28aaa76a61451505ec99055aa87
juke260209-a11y <juke260209@gmail.com>
2026-02-28 21:58:39 +0700

    app.py

--- app.py
+++ app.py
@@ -5,23 +5,20 @@ st.set_page_config(page_title="AI Quản Lý Bài Tập")
 st.markdown("""
 <style>
 .stApp {
-    background: linear-gradient(135deg, #667eea, #764ba2);
+    background-color: #0e1117;
     color: white;
 }
 
 .stTextInput > div > div > input,
 .stNumberInput > div > div > input {
-    background-color: white;
-    color: black;
+    background-color: #1e222a;
+    color: white;
 }
 
 .stButton > button {
-    background-color: #ff4b4b;
-    color: white;
-    border-radius: 10px;
-    height: 45px;
-    width: 200px;
-    font-size: 16px;
+    background-color: #00c8ff;
+    color: black;
+    border-radius: 8px;
 }
 </style>
 """, unsafe_allow_html=True)
@@ -31,11 +28,12 @@ st.title("AI Quản Lý Bài Tập Thông Minh")
 st.write("Nhập thông tin để tính mức độ ưu tiên ")
 
 mon = st.text_input("Tên môn học")
-ngay = st.number_input("Số ngày còn lại", min_value=0)
+ngay = st.number_input("Số ngày còn lại", min_value=1, step=1)
 do_kho = st.slider("Độ khó", 1, 5)
 
 if st.button("Tính ưu tiên"):
-    diem = (6 - do_kho) + (10 - ngay)
+    diem = (do_kho * 2) + (10 / ngay)
+    st.success(f"Điểm ưu tiên: {diem:.2f}")
 
     st.success(f"Điểm ưu tiên của {mon} là: {diem}")
 
@@ -44,3 +42,4 @@ if st.button("Tính ưu tiên"):
     else:
 
         st.info("Có thể làm sau")
+
